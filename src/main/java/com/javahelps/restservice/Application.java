1434f3178065a6c783384e129780e1381689fd3a
MauroLapio <mauroide2011@gmail.com>
2020-05-19 21:52:31 +0200

    fatto a modino

--- src/main/java/com/javahelps/restservice/Application.java
+++ src/main/java/com/javahelps/restservice/Application.java
@@ -1,12 +1,7 @@
 package com.javahelps.restservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.javahelps.restservice.entity.User;
-import com.javahelps.restservice.repository.UserRepository;
 
 @SpringBootApplication
 public class Application
@@ -14,20 +9,5 @@ public class Application
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean
-    protected CommandLineRunner init(final UserRepository userRepository)
-    {
-
-        return args -> {
-            User user = new User();
-            user.setUsername("admin");
-            user.setPassword("admin");
-            user.setName("Administrator");
-            user.setEmail("admin@admin.com");
-            userRepository.save(user);
-
-        };
     }
 }
